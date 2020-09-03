cd9f8f02f681217c3fb2d6f9dcc4d019fa9ad916
jiangkaili <189156395@qq.com>
2020-09-04 00:35:37 +0800

    单链表反转

--- 链表/01-单链表的反转.py
+++ 链表/01-单链表的反转.py
@@ -6,16 +6,13 @@ def reverse_list(head):
         return
     if head.next is None:
         return
-
     p1 = head
     p2 = head.next
     p3 = head.next.next
-
     while not p3:
         p2.next = p1
         p1 = p2
         p2 = p3
         p3 = p3.next
     p2.next = p1
-
     return p2
