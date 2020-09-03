9f9cda6ccfab7fe722bada3212a1e3b2c4531ff4
jiangkaili <189156395@qq.com>
2020-09-03 23:18:31 +0800

    冒泡排序

---
+++ 排序算法/01-冒泡排序.py
@@ -0,0 +1,10 @@
+# author： 蒋开立
+# datetime： 2020/9/3 23:12
+
+
+def bubble_sort(nums):
+    for i in range(len(nums) - 1):
+        for j in range(len(nums) - i - 1):
+            if nums[j] > nums[j+1]:
+                nums[j], nums[j+1] = nums[j+1], nums[j]
+    return nums
